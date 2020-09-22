1ffa55e5084a509e801b4d68c567e033f5b8d0fc
Sofiane Zouaoui <41506121+Sosso8305@users.noreply.github.com>
2020-09-22 16:42:24 +0200

    correction

--- main.c/main.c.ino
+++ main.c/main.c.ino
@@ -7,7 +7,7 @@
   const int led1 = 10;
   const int led2 = 11;
   const int led3 = 12;
-  const int led4 = 13;
+  const int led4 = 9;
 
   int B_ON = 0;
 
@@ -25,45 +25,58 @@ pinMode(led2, OUTPUT);
 pinMode(led3, OUTPUT);
 pinMode(led4, OUTPUT);
 
-digitalWrite(led1,LOW);
-digitalWrite(led2,LOW);
-digitalWrite(led3,LOW);
-digitalWrite(led4,LOW);
+digitalWrite(led1,HIGH);
+digitalWrite(led2,HIGH);
+digitalWrite(led3,HIGH);
+digitalWrite(led4,HIGH);
   
 
 }
 
 void loop() {
+  if (B_ON == 0){
+    if (digitalRead(buzz1)==1){
+      B_ON = 1;
+      Serial.println("led1"); 
+    }
+     if (digitalRead(buzz2)==1){
+      B_ON = 2; 
+    }
+   if (digitalRead(buzz3)==1){
+      B_ON = 3; 
+    }
+   if (digitalRead(buzz4)==1){
+      B_ON = 4; 
+    }
+  }
 
-  if (digitalRead(buzz1)==1){
-    B_ON = 1;
-    Serial.println("led1"); 
-  }
-   if (digitalRead(buzz2)==1){
-    B_ON = 2; 
-  }
- if (digitalRead(buzz3)==1){
-    B_ON = 3; 
-  }
- if (digitalRead(buzz4)==1){
-    B_ON = 4; 
-  }
 
+  if (digitalRead(go)==1){
+      B_ON = 5;
+  } 
+  
   switch (B_ON){
     case 1:
-      digitalWrite(led1,HIGH);
+      digitalWrite(led1,LOW);
       break;
     case 2:
-      digitalWrite(led2,HIGH);
+      digitalWrite(led2,LOW);
       break;
     case 3:
-      digitalWrite(led3,HIGH);
+      digitalWrite(led3,LOW);
       break;
     case 4:
-      digitalWrite(led4,HIGH);
+      digitalWrite(led4,LOW);
+      break;
+    case 5:
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,HIGH);
+      B_ON = 0;
       break;
   }
-  delay(2);
+  delay(1);
 
 
 }
